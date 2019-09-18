6927afc3829f3295e0104b736d6610222d9b0fad
BertrandMoyou <54910102+BertrandMoyou@users.noreply.github.com>
2019-09-18 16:34:28 -0400

    Mon premier Programme

---
+++ 1.Python/300117784.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+@Autho: Bertrand
+
+
+if (4 % 2 == 0):
+    
+   print('pair')      
+   print('ok')
+   else:
+   print('impair')
