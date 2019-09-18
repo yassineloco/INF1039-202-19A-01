286337aa840eb2ca0cb679a529986a9a5d2ebd9e
Auriane25 <55238107+Auriane25@users.noreply.github.com>
2019-09-18 16:30:32 -0400

    mon premier programme python

---
+++ 1.Python/300116670.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+@Author: Auriane
+"""
+if (4%2==0):
+    print ('Pair')
+    print ('ok')
+else:
+    print ('Impair')
