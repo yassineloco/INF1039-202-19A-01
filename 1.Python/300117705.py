5012c66986438b5d05f4bd169247d6319f34ea15
playboikz <54909863+playboikz@users.noreply.github.com>
2019-09-18 16:30:26 -0400

    my new program python

---
+++ 1.Python/300117705.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+@author: Brice
+"""
+if (4 % 2 == 0): 
+    print('pair')
+    print('ok')
+else:
+    ('impair')
+    
