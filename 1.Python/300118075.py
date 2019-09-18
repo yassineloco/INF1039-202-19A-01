0467e015885a781f324b952d7befd4f3a95f3014
root <root@localhost.localdomain>
2019-09-18 16:39:55 -0400

    file 300118075 added

---
+++ 1.Python/300118075.py
@@ -0,0 +1,18 @@
+"""
+lesson1
+
+author@: martialo12
+"""
+
+
+def main():
+    if 4 % 2 == 0:
+        print('pair')
+        print('ok')
+    else:
+        print('impair')
+
+
+if __name__ == '__main__':
+    main()
+
