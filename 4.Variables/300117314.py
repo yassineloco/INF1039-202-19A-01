ebb9d63538e6c9595828808affdbce2c8650f6d1
morti747 <54910700+morti747@users.noreply.github.com>
2019-09-25 16:37:07 -0400

    Create 300117314.py

---
+++ 4.Variables/300117314.py
@@ -0,0 +1,18 @@
+# ma varitable x en memoire
+# type inference
+x=1
+print(2*x)
+# type integer
+x=4
+print(2*x)
+if (x==4):
+    print('x=4')
+else:
+    print('on ne sait pas')
+# type string 
+    a= 'x=4'
+    print(a)
+    
+#double
+d = 2.56
+print (2*8)
