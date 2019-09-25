5fd9ef50d3168545b4d59c84bd104dfa77ba7356
lagokamdem <54910252+lagokamdem@users.noreply.github.com>
2019-09-25 16:36:24 -0400

    Create 300116973.py

---
+++ 4.Variables/300116973.py
@@ -0,0 +1,28 @@
+# ma variable x en memoire
+# Type Inference
+"""
+Created on Wed Sep 25 15:08:34 2019
+
+@author: Admin
+"""
+
+x = 1
+print (2 * x)
+
+# Integrer
+x = 4
+print (2 * x)
+if (x == 4) :
+    print("x = 4")
+else:
+    print ("on ne sait pas")
+a = 'x = 4'
+print(a)
+
+# string
+a = 'x = 4'
+print(a)
+
+#double
+d = 2.56
+print (2 * d)
