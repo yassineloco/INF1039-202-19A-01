54d66cd41b57e1737400572bf036109c4e68ef50
Sekou16 <55207099+Sekou16@users.noreply.github.com>
2019-09-25 16:45:09 -0400

    Create 300111441.py

---
+++ 4.Variables/300111441.py
@@ -0,0 +1,21 @@
+# Ma variable x memoire
+# Type inference
+x=1
+print(2 * x)
+# Integer
+x=4
+print (2 * x)
+if ( x == 4 ):
+    print('x = 4')
+else:
+print('on ne sait')
+# String
+a= 'x = 4'
+print( a )    
+# Double
+d=2.56
+print(2 *d)
+    
+    
+    
+    
