c3c73c37259c3e12eee4e3f8534a7c430d456a67
hamidmallim <54910372+hamidmallim@users.noreply.github.com>
2019-09-25 16:43:17 -0400

    variables 2
    python 2

---
+++ 4.Variables/300116593.py
@@ -0,0 +1,18 @@
+# ma variable x en memoire
+#type Inference 
+x=1
+print(2*x)
+x=4
+print(2*x)
+if(x==4):
+  print('x=4')
+  if (x==4):
+      print('x==4')
+else:      
+      print('on ne sait pas')
+# string
+a ='x=4'
+print(a)
+# Double
+d=2.56
+print(2*d)
