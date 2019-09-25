f59eb37b1827742823a08067cf5879cb94248289
khal1967 <54910842+khal1967@users.noreply.github.com>
2019-09-25 16:42:15 -0400

    Create 300116370.py

---
+++ 4.Variables/300116370.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
+
+# ma variable en memoire
+#type inference
+x=1
+print(2*x)
+#integer
+x=4
+print(2*x)
+if (x==4):
+    print("x=4")
+else:
+    print("on ne sait pas")
+# string
+a="x=4" 
+print(a)
+# double
+d=2.56
+print(2*d)
+
+
+        
+
+
+
+
+
+
+
+      
+
+
+
+    
+   
+    
+
+    
+    
+   
+    
+   
+         
+    
+         
+    
+  
