f046697b3d7636cb28b768862adeafc5bf8e04f0
ndjemou199 <55508292+ndjemou199@users.noreply.github.com>
2019-09-25 14:27:23 -0400

    Create 300118196.PY

---
+++ 1.Python/300118196.PY
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
+if (4 % 3 == 1) :
+    print('impaire')
+    print('le reste de la division est vrai')
+else :
+    print('erreur!')
