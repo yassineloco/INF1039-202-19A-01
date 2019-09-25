cf6ccaff19b9f27ac68e5abe825a7e3f7b534cf3
Zack <54910329+ZackTO@users.noreply.github.com>
2019-09-25 13:59:14 -0400

    My first Phyton language

---
+++ 1.Python/300115140.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+@Author : Zack
+"""
+
+if (5%2 == 0):
+  print ('Pair')
+  print ('ok')
+else:
+  print ('Impair')
+    
