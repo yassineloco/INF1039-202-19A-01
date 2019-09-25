c66e5a86f1444ba0eed12ecc0f4ca50878083a8a
ndjemou199 <55508292+ndjemou199@users.noreply.github.com>
2019-09-25 16:38:37 -0400

    Create 300118196.py

---
+++ 4.Variables/300118196.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 25 15:08:28 2019
+"""
+# déclaration of variable 
+x = 1  
+print(x*2)
+x = 4 
+print(2*x)
+if(x==4):
+    print('x = 4')
+else:
+    print('on ne sait pas') 
+    
+# string
+a = 'x = 4'
+print ( a )
