53d4b29bae793b65e98ad672de08f5acbd3caa26
Wilfridboris <54910937+Wilfridboris@users.noreply.github.com>
2019-09-25 13:41:35 -0700

    Create 300117178.py

---
+++ 4.Variables/300117178.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+ma variable en memoire
+type 
+"""
+
+x=1
+print(2*x)
+x=4
+print(2*x)
+if(x==4):
+    print('x=4')
+else:
+    print('on ne sait pas')
+a='x=4'
+print(a)
+d=2.6
+print(2*d)
