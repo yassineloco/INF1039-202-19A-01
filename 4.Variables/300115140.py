fb4a6d9d8030e12d264f014267f511fbfdd39455
Zack <54910329+ZackTO@users.noreply.github.com>
2019-09-25 16:41:39 -0400

    Variable

---
+++ 4.Variables/300115140.py
@@ -0,0 +1,22 @@
+# Ma variable en mémoire
+# Type Inference
+x=1
+print(2*x)      
+# Integer
+x=4
+print(2*x)
+
+if (x == 4):
+    print('x = 4')
+else:
+    print('on ne sait pas')
+a = 'x = 4'
+print ( a  )    
+
+#string
+a = 'x = 4'
+print ( a )
+
+# Double
+d = 2.56
+print (2 * d)
