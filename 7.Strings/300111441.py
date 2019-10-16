5a0cb3f462fd3b1fd316b19ee89f8e52f83705a5
Fofana Sekou <sko.fofana@gmail.com>
2019-10-16 13:33:16 -0400

    added neew file

---
+++ 7.Strings/300111441.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  9 15:16:03 2019
+
+@author: skofo
+"""
+
+# creer une chaine variable s contenant la chaine ma chaine
+s = 'ma chaine'
+print(s)
+
+# Afficher le remier caractere
+print(s[0])
+
+# Afficher le caractere i
+print(s[6])
+
+# afficher le chaine ai dans la chaine ma chaine
+print(s[5:8:2])
