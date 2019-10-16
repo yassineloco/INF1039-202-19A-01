80fca0a791babbb2ea7b605f0773bc6f25ca16e2
unknown <lagokamdem87@gmail.com>
2019-10-16 14:40:25 -0400

    nouveau fichier ajouter

---
+++ 7.Strings/300116973.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  9 15:21:47 2019
+
+@author: dell
+"""
+# creer une variable s contenant la chaine de caractere
+s = "ma chaine"
+print(s)
+
+# Afficher le caratere i
+print(s[6])
+# afficher le premier caractere
+
+print(s[0])
+
+# afficher le caractere c
+print(s[3])
+
+#afficher la chaine de caractere ai dans la chaine 'ma chaine'
+print(s[5:7:1])
+
+# afficher la chaine de caractere a et m dans la chaine 'ma chaine'
+print(s[5:8:2])
+
