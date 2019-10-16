cd1c83d50887d5c7f8f4c494a10dd8adc14d204e
root <root@localhost.localdomain>
2019-10-16 15:00:41 -0400

    new file added!

---
+++ 8.Tuples/300118075.py
@@ -0,0 +1,20 @@
+"""
+author@: martialo12
+"""
+
+
+def main():
+    """
+     strings
+     :return:
+     """
+    # Rechercher la le charactère 'e' dans la vriable who
+    who = "io io stesso e io"
+    position = who.find('e')
+
+    print('e se trouve à la position: {}'.format(position))
+
+
+if __name__ == '__main__':
+    main()
+
