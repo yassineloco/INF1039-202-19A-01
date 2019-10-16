c5e85b8a772c8ed9d41613a97c75ced3c1476d48
playboikz <acorlings@gmail.com>
2019-10-16 15:00:19 -0400

    haert

---
+++ 7.Strings/300117705.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  9 15:44:36 2019
+
+@author: Surface pro
+"""
+# creer une variable s contenant le chaine ma chaine 
+s = 'ma chaine'
+print (s)
+
+# afficher le premier caractere
+print(s[0])
+
+# afficher le caractere i 
+print(s[6])
+
+# afficher la chaine de caractere ai dans la chaine ma chaine 
+print(s[5:8:2])
+
+
+
