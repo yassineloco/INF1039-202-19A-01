24ec011b1731da783daf94c26fcc28bece628472
amirali175 <amirdek511@gmail.com>
2019-10-29 17:11:30 -0400

    excellence

---
+++ 6.Capstone-I/300116685.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 29 15:49:20 2019
+
+@author: Amir
+"""
+# declaration de la variable heure par l'utilisateur 
+hour = int(input("veuillez inserer un entier positif: "))
+
+#convertion de heur en minute
+minute = hour*60
+
+print ('la convertion de', hour, ' heures en minute egale: ', minute,'min')
