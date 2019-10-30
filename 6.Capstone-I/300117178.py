15c5c6384be95261057d64ab7cc6078365e70d89
willfrid boris <boriskwayep35@yahoo.com>
2019-10-30 13:35:40 -0700

    yes

---
+++ 6.Capstone-I/300117178.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 30 10:55:05 2019
+
+@author: willfrid boris
+"""
+
+#insere l'heure a convertir
+heures=int(input('inserer heure'))
+minute=heures*60
+
+#afficher l'heure
+print(minute)
+print('heure=',minute)
