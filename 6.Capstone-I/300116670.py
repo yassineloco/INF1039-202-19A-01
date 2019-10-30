cac41f832b2d1196df089d521d494f23b7451837
Auriane25 <55238107+Auriane25@users.noreply.github.com>
2019-10-30 13:37:04 -0400

    Create 300116670.py

---
+++ 6.Capstone-I/300116670.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 30 12:19:54 2019
+
+@author: K-D Marie Auriane
+"""
+
+#programme pour convertir les heures en minute
+
+#variable x designant le nombre d'heures
+#pour afficher la valeur a la ligne (\n)
+heures=int(input('entrez une valeur: \n'))
+
+minutes= heures*60
+
+print(minutes)
