6faadd68df453801a589504e7654fb6ab784f048
ZackTO <zackstoronto@gmail.com>
2019-10-30 16:32:27 -0400

    NIMPORTE QUOI

---
+++ 9.Conditions/300115140.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 30 14:37:23 2019
+
+@author: zacks
+"""
+
+num = int(input("Enter a number:"))
+if num > 0:
+    print("num is positive")  
+else :   
+    print ("negatif")
+    
