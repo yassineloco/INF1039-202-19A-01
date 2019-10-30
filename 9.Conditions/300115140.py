746d887f84ca14c5c445db4fc3f1eecda116aba9
ZackTO <zackstoronto@gmail.com>
2019-10-30 17:27:38 -0400

    bye

--- 9.Conditions/300115140.py
+++ 9.Conditions/300115140.py
@@ -1,13 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct 30 14:37:23 2019
+Created on Wed Oct 30 17:25:56 2019
 
 @author: zacks
 """
 
-num = int(input("Enter a number:"))
-if num > 0:
-    print("num is positive")  
-else :   
-    print ("negatif")
-    
