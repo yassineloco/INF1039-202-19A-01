401860a4fe9b117c55130b554ccae7ab26004e4b
josephabnerremy <remyabner3@gmail.com>
2019-11-02 14:18:03 -0400

    feu

---
+++ 4.Variables/300065026.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  2 14:11:28 2019
+
+@author: Jeremie
+"""
+# ma variable x en memoire
+
+
+# Type Inference
+
+x = 1
+print(2 * x)
+
+# Integer
+x = 6
+print(2 * x)
+if (x == 6):
+    print('x = 6')
+    print('ok')
+else:
+    print('on ne sait pas')
+
+# String
+a = 'x = 6'
+print( a ) 
+
+# Double
+d = 4.35
+print(2 * d)
