b61298d9618dc3b7726aedc319fa2abe71ec0e66
lagokamdem <54910252+lagokamdem@users.noreply.github.com>
2019-11-06 14:32:25 -0500

    Create 300116973.py

---
+++ 9.Conditions/300116973.py
@@ -0,0 +1,13 @@
+
+Created on Wed Nov  6 14:28:07 2019
+
+@author: nathalie
+"""
+
+# afficer une valeur
+x = int(input('entrer un nombre: '))
+
+if (x < 0):
+    print('x est negatif')    
+else:
+     print('x est positif')
