57acce16c61b7c5b7f28cea26644ee0b200f7a5b
Djibo648 <hassanag70@gmail.com>
2019-11-06 17:26:34 -0500

    moi dans l'informatique

---
+++ 7.Strings/300117806.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  6 17:10:53 2019
+
+@author: 300117806
+"""
+# type interference#
+X = 1
+print('2 * X')
+if (X == 6):
+    print('X = 6')
+    print('ok')
+else:
+    print('a')
+    # Double
+    d = 4.35
+    print('2 * d')
+    
