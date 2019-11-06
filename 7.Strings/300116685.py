e9cdf884f01ab4fb94670b268a5fc086322afecd
amirali175 <amirdek511@gmail.com>
2019-11-06 14:54:22 -0500

    me programmer

--- 7.Strings/300116685.py
+++ 7.Strings/300116685.py
@@ -5,9 +5,7 @@ Created on Wed Oct  9 15:46:07 2019
 @author: Amir
 """
 
-creer une variable  s contenant le chaine
-s= 'ma chaine' 
-print(s)
+#creer print(s)
 
 #Affiche le premier caractere 
 
@@ -16,4 +14,6 @@ print(s[0])
 print(s[6]) 
 
 #Affiche la chaine de caractere 'ai' dans la chaine 'ma chaine'
-print(s[6:5:7])
+print(s[5:7:1])
+#Affiche la caractere 'an' dans la chaine 'ma cahine ' 
+print(s[])
