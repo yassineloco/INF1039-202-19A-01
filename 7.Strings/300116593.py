baf155d995351628eba8f16817f5af91bd89e2f3
hamidmallim <54910372+hamidmallim@users.noreply.github.com>
2019-11-06 14:03:00 -0500

    300116593.py
    souley

---
+++ 7.Strings/300116593.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  6 13:58:57 2019
+
+@author: Hi
+"""
+
+#creer une variable s contenant la chaine 'ma chaine'
+s = 'ma chaine'
+print (s)
+
+# Afficher le premier caractere 
+print(s[0])
+
+#afficher le caractere i
+print(s[6])
+
+# afficher la chaine de caractere 'an' dans la chaine 'ma chaine'
+print(s[5:8:2])
