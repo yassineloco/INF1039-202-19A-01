4a40a95ec1c6b651f0a4f629f204526128311ea0
zoureni <zoureni@outlook.fr>
2019-11-11 17:40:33 -0500

    zoureni

---
+++ 1.Python/300118524.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 11 17:37:42 2019
+
+@author: zoureni
+"""
+
+if (4109 % 2 == 0):
+    print('pair')
+else: 
+    print('impair')
