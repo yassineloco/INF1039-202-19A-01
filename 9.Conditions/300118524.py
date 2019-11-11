68f68b638ffd70dcb5c8f523fcb563882b0b055b
zoureni <zoureni@outlook.fr>
2019-11-11 17:48:12 -0500

    zoureni

--- 9.Conditions/300118524.py
+++ 9.Conditions/300118524.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Nov  6 14:27:30 2019
+Created on Mon Nov 11 17:44:18 2019
 
 @author: zoureni
 """
 
-3 + 2
+num=int(input("Entrer votre nombre pour vérification: "))
+
+if num>=0:
+    print('votre nombre est positif')
+else:
+    print('votre nombre est negatif')
