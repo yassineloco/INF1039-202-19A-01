58869588089a7112e391c1abee9f197fd52f6ad8
dido05327 <54910261+dido05327@users.noreply.github.com>
2019-11-26 18:29:34 -0500

    String

---
+++ 7.Strings/300117444.py
@@ -0,0 +1,21 @@
+Created on Tue Nov 06 18:24:03 2019
+
+@author: idira
+"""
+
+
+# creer une variable Ch contenant le chaine ma chaine 
+ch = 'Tombouctou'
+#afficher tout le caractere 'Tombouctou'
+print (ch)
+
+# afficher le premier caractere(T).
+print(ch[0])
+
+# afficher le caractere t 
+print(ch[7])
+
+# afficher la chaine de caractere "ob" dans la chaine ma chaine 
+print(ch[1:5:2])
+# afficher la chaine de caractere 'uc' dans la chaine 'ma chaine'
+print(ch[5:7:1])
