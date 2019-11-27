6e5283ef119183fec2a17cb6d9a3cab0c001d740
franck1999-30 <christinnguekam@gmail.com>
2019-11-27 16:38:23 -0500

    interressant :blush:

---
+++ C.Loops/300118196.py
@@ -0,0 +1,7 @@
+#-*-coding: utf-8 -*-
+""" travail sur les boucles"""
+
+christin = [1,2,3,4,5,8,7,9]
+print ("nous obtenons de maniere croissante le resultat suivant:")
+for v in christin:
+     print (v)
