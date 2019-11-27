c2dcab3d84a2c512aaaddd9a280219c8eb66ceaa
fadde68 <54910778+fadde68@users.noreply.github.com>
2019-11-27 18:16:25 -0500

    Create 300115065.py

---
+++ A.Capstone-II/300115065.py
@@ -0,0 +1,22 @@
+
+@author: akram
+"""
+
+#ecrire le variable a l'entrer
+NOM1 = input('nom prenom:')
+#2e variable a l'entrer
+NOM2 = input('nom prenom:')
+#3e variable a l'entrer
+
+
+#choisir les variables ou caracteres
+
+H= (NOM1[2:6]+ NOM2[4:8])
+K= (NOM1[2:8] + NOM2[2:5])
+
+
+#afficher les caracteres 
+
+print (H)
+
+print(K)
