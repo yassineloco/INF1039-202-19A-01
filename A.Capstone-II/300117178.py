c2f311df4dc724376127806fe63f515388b5c95f
willfrid boris <boriskwayep35@yahoo.com>
2019-11-30 21:13:20 -0800

    essage:whale2:

--- A.Capstone-II/300117178.py
+++ A.Capstone-II/300117178.py
@@ -5,51 +5,20 @@ Created on Wed Nov  6 12:38:07 2019
 @author: willfrid boris
 """
 
-
-n="""
-
-   CE PETIT PROGRAMME FAIT LA COMBINAISON DE VOS DEUX PREMIER PRENOM ET VOS DEUX PREMIER NOMS
-
-   VEUILLEZ ENTREZ VOS DEUX PREMIERS PRENOMS ET PRENOM
-
-   SVP
-
-   """
-
-print(n)
-
-p1=str(input("PRENOM:\n"))
-
-p2=str(input(""))
-
-p3=str(input("NOM:\n"))
-
-p4=str(input(""))
-
-s1=p1[0:4]+p2[0:4]
-
-s2=p3[0:4]+p4[0:4]
-
-print('',s1,'',s2)
-
-print("VOULEZ VOUS SAISIR A NOUVEAU O/N ?")
-
-q=str(input("\n"))
-
-if(q=='o'):
-
-    p1=str(input("PRENOM:\n"))
-
-    p2=str(input(""))
-
-    p3=str(input("NOM:\n"))
-
-    p4=str(input(""))
-
-    s1=p1[0:4]+p2[0:4]
-
-    s2=p3[0:4]+p4[0:4]
-
-else:
-
-    print("aurevoir")
+p1=str(input("PRENOM:"))
+p3=str(input("NOM:"))
+if(len(p1)>10 and len(p3)>10):
+    s1=p1[0:3]+p1[9:12]
+    s2=p3[0:2]+p3[9:12]
+if(len(p1)>10 and len(p3)<10 ):
+    s1=p1[0:3]+p1[9:12]
+    s2=p3[0:2]+p3[6:9]
+if(len(p1)<10 and len(p3)>10 ):
+    s1=p1[0:3]+p1[6:9]
+    s2=p3[2:5]+p3[9:12]
+if(len(p1)<10 and len(p3)<10 ):
+    s1=p1[0:3]+p1[6:9]
+    s2=p3[0:3]+p3[6:9]
+print('#####################')
+print('Mashup:',s1,'',s2)
+print('#####################')
