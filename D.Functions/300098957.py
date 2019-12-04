00ef8825f876499bbdf20184477e0c4c8c64110f
Setrar <setrar@valiha.com>
2019-12-04 16:06:15 -0500

    .

--- D.Functions/300098957.py
+++ D.Functions/300098957.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Dec  4 11:09:15 2019
-@author: willfrid boris
-"""
-def f(x):
-   return  x+1
+Created on Wed Dec  4 14:18:41 2019
 
-i=0
-while(i<10):
-    i=i+1
-    print(f(i))
+@author: 300098957
+"""
+
+import math
+
+def f( x):
+    return x + 1
+    
+i = 1
+while i < 10:
+    print(math.sqrt(f(i)))
+    i = i + 1
