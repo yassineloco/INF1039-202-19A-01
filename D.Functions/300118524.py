0bce2725c8a4ec22f3755260aa79a9471be00854
zoureni <zoureni@outlook.fr>
2019-12-04 16:16:25 -0500

    zoureni

--- D.Functions/300118524.py
+++ D.Functions/300118524.py
@@ -8,5 +8,14 @@ Created on Wed Dec  4 14:02:34 2019
 
 def f(x):
     return x + 1
-
-print(f(1))
+i=0
+while(i <= 10):
+    i=i+1
+    
+    print(f(i))
+  
+i=0
+while(i <= 10):
+    i=i+1
+    import math 
+    print(math.sqrt(f(i)))
