1cac7b8432eb49b3defb55a5908b76e65a9b9795
dido05327 <54910261+dido05327@users.noreply.github.com>
2019-12-04 14:37:53 -0500

    Fonction

---
+++ D.Functions/300117444.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  4 14:19:46 2019
+
+@author: idirAbib
+"""
+#faire une fonction f(x)
+
+def f(x) :
+    return x + 1     # returner la valeur de  x + 1 
+print(f(2))  # afficher le resultat ( 1+2 = 3 sur se cas ici)
