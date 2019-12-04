1c3ca1442266012bfebb630f90c3c31dfefcce5f
Sekou16 <sko.fofana@gmail.com>
2019-12-04 16:37:36 -0500

    k

--- D.Functions/300111441.py
+++ D.Functions/300111441.py
@@ -4,18 +4,11 @@ Created on Wed Dec  4 14:10:26 2019
 
 @author: skofo
 """
+import math
 
 def f( x):
     return x + 1
-  
-#creer le while     
-
-a=1
-while (a <= 10):
-
-   print(f(a)) 
-   a=a+1
-   
-#Imprimez racine de carre f(a)
-
-print(f(a)** 0.5)    
+a = 1
+while a < 10:
+    print(math.sqrt(f(a)))
+    a= a + 1
