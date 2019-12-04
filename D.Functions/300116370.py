a985e1d5d3c59c145baa5c99ef76b0f0ad2cbb92
khal1967 <khalidouahhabi@gmail.com>
2019-12-04 16:46:48 -0500

    whale

--- D.Functions/300116370.py
+++ D.Functions/300116370.py
@@ -8,5 +8,17 @@ Created on Wed Dec  4 13:56:15 2019
 def f(x):
     y =x+1
     return y
-print(f(1))  
+print(f(1))
+
+
+i = 1
+
+while(1 < 10):
+    print(i+10)
+    
+    
+        
+        
+        
+        
   
---
+++ D.Functions/370116370.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  4 15:37:57 2019
+
+@author: khali
+"""
+
+def f(x):
+    y =x+1
+    return y
+ 
+
+i= 0
+while i <=10 :
+    i=i+1
+    
+    print(f(i))
+        
+i = 1
+while i < 10 :
+    
+    i=i+1
+    from math import sqrt
+    print(sqrt(f(i)))
+    
+    
