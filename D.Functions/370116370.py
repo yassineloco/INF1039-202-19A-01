b91ec5754ca0d4c824f901d041849d3a85876e6f
khal1967 <54910842+khal1967@users.noreply.github.com>
2019-12-04 20:57:15 -0500

    Update 370116370.py

--- D.Functions/370116370.py
+++ D.Functions/370116370.py
@@ -7,20 +7,21 @@ Created on Wed Dec  4 15:37:57 2019
 
 def f(x):
     y =x+1
-    return y
- 
+    return y  #retourner la valeur de y
+    print(f(1))  # afficher le resultat 
 
 i= 0
-while i <=10 :
+while i < 10 :   # tant que iest inferieur a zero
     i=i+1
     
-    print(f(i))
+    print(f(i))  #imprimer le resultat de i
         
 i = 1
 while i < 10 :
     
     i=i+1
-    from math import sqrt
-    print(sqrt(f(i)))
+    from math import sqrt   #importer racine carree
+    print(sqrt(f(i)))       #imprimer le resultat
+    
     
     
