4d441e5a04ebfd667109d1719ee2fe0ba4148bbd
hamidmallim <54910372+hamidmallim@users.noreply.github.com>
2019-12-08 10:04:35 -0500

    Create 300116593.py

---
+++ D.Functions/300116593.py
@@ -0,0 +1,27 @@
+#  calcul de fonction f
+def f( x):
+  return  x+1
+x=1
+print(f(1))
+
+# chercher un resultat qui sera inerieur ou egale a 10.
+n =0
+
+while n <=10:
+    
+    n = n+1 
+   
+    
+    print (n)
+
+#le resultat la racine
+
+
+x = 0
+
+
+while x <=10:
+    
+    x = x + 1
+    from math import sqrt
+    print(sqrt(f(x)))
