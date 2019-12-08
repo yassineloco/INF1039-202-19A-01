bab910754ab67dd97b3e9f57722be27e453272ab
hamidmallim <54910372+hamidmallim@users.noreply.github.com>
2019-12-08 09:32:39 -0500

    Update and rename 3001165930.py to 300116593.py
    mon programme

---
+++ 9.Conditions/300116593.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 30 14:37:28 2019
+
+@author: Souleymane
+"""
+num= int(input("entrer un num:"))
+
+# afficher le nombre inferieur a zero
+
+if num<0:
+    print("num est inferieur a zero")
+    
+# afficher nombre si superieur a zero
+else:
+    print("num superieur a 0") 
